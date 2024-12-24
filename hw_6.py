b4f015ca2e720575cec15f7566c9cbca21153fe3
ILGIZ AKBARALIEV <okoo.goe@gmail.com>
2024-12-25 02:17:09 +1100

    hw_6

---
+++ hw_6.py
@@ -0,0 +1,39 @@
+from json.decoder import PosInf
+
+
+def binerry_search(a,val):
+    N = len(a)
+    ResultOk = False
+    Pos = 0
+    First = 0
+    Last = N - 1
+    while First < Last:
+            Middle = (First+Last) // 2
+            if val == a[Middle]:
+                First = Middle
+                Last = First
+                ResultOk = True
+                Pos = Middle
+            elif val > a[Middle]:
+                First = Middle + 1
+            else:
+                Last = Middle - 1
+    if ResultOk == True:
+        print(f"Element is find {Pos}")
+    else:
+        print(f"element is doesn't found")
+topic =[1,2,3,4,5,6,7,8,9]
+number = 2
+binerry_search(topic,number)
+
+
+def bubble_sort(human):
+    for q in range(len(human) - 1):
+        for m in range(len(human) - 1 - q):
+            if human[m] > human[m + 1]:
+                word  = human[m]
+                human[m] = human[m + 1]
+                human[m + 1] = word
+    return human
+
+print(bubble_sort([9, 8, 7, 6, 5, 4, 3, 2, 1]))
